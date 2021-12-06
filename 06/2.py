51350a49c83f65b0e712e82497462b7aa90eb8ad
Chris Kamphuis <mail@chriskamphuis.com>
2021-12-06 09:43:01 +0100

    shorter day 6

--- 06/2.py
+++ 06/2.py
@@ -1,14 +1,6 @@
-from collections import defaultdict
-
-states = defaultdict(int)
+states = [0 for _ in range(9)]
 for e in open('input.txt', 'r').read().strip().split(','):
     states[int(e)] += 1
-
-for _ in range(256):
-    new_states = defaultdict(int)
-    new_states[8] = states[0]
-    new_states[6] = states[0]
-    for i in range(1, 9):
-        new_states[i-1] += states[i]
-    states = new_states
-print(sum([v for k,v in states.items()]))
+for i in range(256):
+    states[(7+i)%9] += states[i%9] 
+print(sum(states))
