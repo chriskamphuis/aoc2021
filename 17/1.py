6a38d2a51ee6232a539a87f40403df7e34d19e71
Chris Kamphuis <mail@chriskamphuis.com>
2021-12-17 14:51:24 +0100

    day 17

---
+++ 17/1.py
@@ -0,0 +1,29 @@
+(xmin, xmax), (ymin, ymax) = [
+            [int(a) for a in e.split('=')[-1].split('..')] 
+            for e in open('input.txt', 'r').read().strip().replace(',', '').split(' ')[-2:]
+        ]
+
+def trajectory(dx, dy, xmax, ymax):
+    x, y = 0, 0
+    while x < xmax and y > ymin:
+        x += dx
+        y += dy
+        yield x, y
+        if dx > 0: dx -= 1
+        if dx < 0: dx += 1
+        dy -= 1
+
+any_in_target_area = lambda t, xmin, xmax, ymin, ymax: any([xmin<=x<=xmax and ymin<=y<=ymax for x, y in t])
+
+dx, dy = xmax, (xmax//2)+1
+while True:
+    t = trajectory(dx, dy, xmax, ymax)
+    if any_in_target_area(t, xmin, xmax, ymin, ymax):
+        t = trajectory(dx, dy, xmax, ymax)
+        print(max((y for x,y in t)))
+        break
+    dx -= 1
+    if dx == 0:
+        dx = xmax
+        dy -= 1
+
