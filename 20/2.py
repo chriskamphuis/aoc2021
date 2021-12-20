26be0fb257d89792a9687a5c68593a1101a8e0ed
Chris Kamphuis <mail@chriskamphuis.com>
2021-12-20 16:33:44 +0100

    day 20

--- 20/2.py
+++ 20/2.py
@@ -6,7 +6,7 @@ algorithm, data = open('input.txt').read().strip().split('\n\n')
 algorithm = [1 if c =='#' else 0 for c in algorithm.strip()]
 data = np.array([[1 if c =='#' else 0 for c in line] for line in data.strip().split('\n')])
 data = np.pad(data, 3, constant_values=0)
-for it in range(2):
+for it in range(50):
     data = np.pad(data, 2, mode='reflect')
     new_data = np.zeros((data.shape[0]-2, data.shape[1]-2))
     for x in range(data.shape[0]-2):
